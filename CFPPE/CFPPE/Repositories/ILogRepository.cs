3e9d07e4f399764c2121b9a4bc9738a5b42fd372
jhoonyy zzssooliisszz <jhonny_gs94@hotmail.com>
2020-09-15 18:28:26 -0500

    complementando m1

---
+++ CFPPE/CFPPE/Repositories/ILogRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using CFPPE.Models;
+
+namespace CFPPE.Repositories
+{
+	public interface ILogRepository
+	{
+		int GuardaLog(int usuario, string pantalla);
+
+		/// <summary>
+		/// Busqueda a BD por fecha inicial, fecha final y por usuario en Log
+		/// </summary>
+		/// <param name="f1"></param>
+		/// <param name="f2"></param>
+		/// <returns>Lista de consulta con criterios en Accesos</returns>
+
+		List<usuario> getAccesoCriterio(string f1, string f2, int usuario);
+	}
+}
